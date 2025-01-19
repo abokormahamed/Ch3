72b66ad263c3cfd2210dee8ee0b687361af512da
abokor.osman <abokor.osman03@gmail.com>
2025-01-19 18:13:19 +0100

    pusher opp kodene

---
+++ Welcome4/Welcome4/Welcome4.cs
@@ -0,0 +1,13 @@
+﻿// Fig. 3.13: Welcome4.cs
+// Inserting content into a string with string interpolation.
+using System;
+
+class Welcome4
+{
+   // Main method begins execution of C# app
+   static void Main()
+   {
+      string person = "Paul"; // variable that stores the string "Paul"
+      Console.WriteLine($"Welcome to C# Programming, {person}!");
+   } // end Main
+} // end class Welcome4
